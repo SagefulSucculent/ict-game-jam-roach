d8b9b6382baffaed4ba6f5aaad2191f2f95a0131
Aidan Mannette <52185882+amannette@users.noreply.github.com>
2024-07-20 17:55:09 -0500

    Update script.js
    extend scene object

--- script.js
+++ script.js
@@ -1,51 +1,16 @@
-var config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: false,
-            debug: false
-        }
-    },
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
-};
+//roach game!
+
+
 
 var player;
-var stars;
-var bombs;
-var platforms;
+var spider;
 var cursors;
-var score = 0;
 var gameOver = false;
-var scoreText;
-
-var game = new Phaser.Game(config);
 
 
 
-function preload ()
-{
-    this.load.image('sky', 'assets/sky.png');
-    this.load.image('roach','assets/roach_32.png')
-
-    ///////scenery
-    this.load.image('can', 'assets/can_32.png');
-    this.load.image('leaf_s', 'assets/leaf_32.png');
-    this.load.image('leaf_l', 'assets/leaf_64.png');
-    this.load.image('rock', 'assets/rock_48.png');
-    this.load.image('stone', 'assets/stone_96.png');
-
-}
-
-
-const getRandCoord = () => {
-    return new Phaser.Math.Vector2(600*Math.random(),800*Math.random())
+const getRandCoord = (ctx) => {
+    return new Phaser.Math.Vector2(800*Math.random(),600*Math.random())
 }
 var ItemNames = ['stone','rock','leaf_l','leaf_s','can']
 var NumItems = 8
@@ -55,48 +20,11 @@ const genItems = (ctx) => {
     {
         let pos = getRandCoord()
         ctx.items.push(ctx.add.image(pos.x,pos.y, ItemNames[Math.floor(Math.random()*5)]))
-    }
-}
-
-function create ()
-{
-    //  A simple background for our game
-    this.items = []
-
-    this.add.image(400, 300, 'sky');
-    genItems(this);
-    //console.log(this.items)
-    this.items.forEach(i=>{
-        i.angle = 360 * Math.random()
-        console.log(i.angle)
         
-    })
 
-    player = this.physics.add.image(100, 450, 'roach');
-
-
-    //  Player physics properties. Give the little guy a slight bounce.
-    player.setBounce(0.2);
-    player.setCollideWorldBounds(true);
-
-
-
-    //  Input Events
-    cursors = this.input.keyboard.createCursorKeys();
-
-}
-
-
-function update ()
-{
-    if (gameOver)
-    {
-        return;
     }
-
-    moveSystem()
-
 }
+
 
 const moveSystem = () => {
     if((game.loop.frame)%30 == 0)
@@ -118,5 +46,84 @@ const moveSystem = () => {
 
     }
 
+}
+
+class MainScene extends Phaser.Scene 
+{
+    constructor ()
+    {
+        super();
+    }
+
+    preload ()
+    {
+        this.load.image('sky', 'assets/sky.png');
+        this.load.image('roach','assets/roach_32.png')
+        this.load.image('spider','assets/spider_64.png')
+
+        ///////scenery
+        this.load.image('can', 'assets/can_32.png');
+        this.load.image('leaf_s', 'assets/leaf_32.png');
+        this.load.image('leaf_l', 'assets/leaf_64.png');
+        this.load.image('rock', 'assets/rock_48.png');
+        this.load.image('stone', 'assets/stone_96.png');
+
+
+        ////shader
+        this.load.glsl('fire','assets/shader/shader0.frag');
+
+    }
+
+ create ()
+{
+
+    this.items = []
+    
+    this.add.image(400, 300, 'sky');
+    genItems(this);
+    this.items.forEach(i=>{
+        i.angle = 360 * Math.random()
+        console.log(i.angle)      
+    })
+
+    player = this.physics.add.image(100, 450, 'roach');
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    spider = this.physics.add.image(300, 150, 'spider');
+    spider.setCollideWorldBounds(true);
+
+    this.physics.add.collider(player, spider);
+
+    //  Input Events
+    cursors = this.input.keyboard.createCursorKeys();
+}
+
+
+ update ()
+{
+    if (gameOver)
+    {
+        return;
+    }
+
+    moveSystem()
+}
+
 
 }
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: false,
+            debug: false
+        }
+    },
+    scene: MainScene
+};
+
+var game = new Phaser.Game(config);
